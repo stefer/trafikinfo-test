95469d7be4257a6211c8370d9aedd7ec70101286
Stefan Eriksson <stefan.piaostefan@gmail.com>
2014-11-26 00:21:16 +0100

    Show station names in "Till" field.

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,4 +1,5 @@
-angular.module('Trafikinfo.controllers', [])
+angular
+.module('Trafikinfo.controllers', [])
     .controller('stationsController', function ($scope, TrafikinfoAPIService) {
         $scope.stations = [];
         $scope.announcements = [];
@@ -10,7 +11,20 @@ angular.module('Trafikinfo.controllers', [])
             });
         }
         
+        $scope.getStation = function (stationId) {
+            return ($scope.stations || []).find(function(element, index, array) {
+                return element.LocationSignature === stationId;
+            });
+        }
+        
         TrafikinfoAPIService.getStations().success(function(response) {
             $scope.stations = response.RESPONSE.RESULT[0].TrainStation;
         });
-});
+})
+.filter('stationIdsToNames', function () {
+    return function (input, scope) {
+        return (input || [])
+        .map(function(stationId) {return scope.getStation(stationId).AdvertisedLocationName})
+        .join(', ');
+    };
+});;
