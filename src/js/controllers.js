fd6a1c2c97ba665881d0b275a759d8b30e377138
Stefan Eriksson <stefan.piaostefan@gmail.com>
2014-12-06 19:20:05 +0100

    Use Bower

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,22 +1,21 @@
-angular
-.module('Trafikinfo.controllers', [])
+angular.module('Trafikinfo.controllers', [])
     .controller('stationsController', function ($scope, TrafikinfoAPIService) {
         $scope.stations = [];
         $scope.announcements = [];
         $scope.selectedStation = undefined;
-    
+
         $scope.getAnnouncements = function (stationSign) {
             TrafikinfoAPIService.getAnnouncements(stationSign).success(function (response) {
                 $scope.announcements = response.RESPONSE.RESULT[0].TrainAnnouncement;
             });
         }
-        
+
         $scope.getStation = function (stationId) {
             return ($scope.stations || []).find(function(element, index, array) {
                 return element.LocationSignature === stationId;
             });
         }
-        
+
         TrafikinfoAPIService.getStations().success(function(response) {
             $scope.stations = response.RESPONSE.RESULT[0].TrainStation;
         });
